81673509cc1cfccced7bf94388425e308df9ce9d
Maykel Arias Torres <hanibal920915@gmail.com>
2022-03-22 20:26:22 -0400

    Corrigiendo plantillas

--- main.py
+++ main.py
@@ -6,13 +6,7 @@ from jinja2 import Environment, FileSystemLoader
 
 
 def main():
-
-    # Obtener el argumento
-    print('Number of arguments:', len(sys.argv), 'arguments.')
-    print('Argument List:', str(sys.argv))
-
     # Capturar el ID del product_global
-    # id_product = "51328465"
     id_product = sys.argv[1]
 
     # declarar donde está la plantilla
@@ -21,26 +15,27 @@ def main():
     template = env.get_template('SendZoho.html')
 
     # declara el API a consumir
-    reqUrl = "https://api.kieroapi.net/variations/product_global/"+id_product
+    # req_url = "https://api.kieroapi.net/variations/product_global/" + id_product
+    req_url = "https://api.kieroapi.net/variations/product_funnel/" + id_product
     headersList = {}
     payload = {}
 
     # Obtener valores del api para construir la plantilla
-    response = requests.get(reqUrl, data=payload,  headers=headersList)
+    response = requests.get(req_url, data=payload, headers=headersList)
 
     # Convertir respuesta del API a JSON
     data = response.json()
     # Hacer la operación siempre que este presente data
     if response.status_code == 200 and 'data' in data:
         data_render = data['data']
-
         # Truncar la description a solo 254
-        data_render['description'] = data['data']['description'][:254]
-
+        data_render['product_description'] = data['data']['product_description'][:254]
+        if data['data']['product_description']:
+            data_render['active'] = "true"
+        else:
+            data_render['active'] = "false"
         # Mandar los datos y construir los datos
         output = template.render(data=data_render)
-        # print(output)
-
         # declara donde generar el HTML
         filename = f"generada/SendZoho-{id_product}.html"
         if not os.path.exists("generada"):
