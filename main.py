4d13f2c8604565459e133962a04599afe098b55d
Maykel Arias Torres <hanibal920915@gmail.com>
2022-04-07 14:29:36 -0400

    Primera correction

--- main.py
+++ main.py
@@ -16,19 +16,18 @@ def main():
 
     # declara el API a consumir
     req_url = "https://api.kieroapi.net/variations/product_funnel/" + id_product
-    req_url_2 = "https://api.kieroapi.net/variations/product_global/" + id_product
     headersList = {}
     payload = {}
 
     # Obtener valores del api para construir la plantilla
     response = requests.get(req_url, data=payload, headers=headersList)
-    response_2 = requests.get(req_url_2, data=payload, headers=headersList)
+
 
     # Convertir respuesta del API a JSON
     data = response.json()
-    data_2 = response_2.json()
+
     # Hacer la operación siempre que este presente data
-    if response.status_code == 200 and 'data' in data and 'data' in data_2:
+    if response.status_code == 200 and 'data' in data:
         data_render = data['data']
         # Truncar la description a solo 254
         data_render['product_description'] = data['data']['product_description'][:254]
@@ -38,15 +37,15 @@ def main():
             data_render['active'] = "false"
         # Crear el breadcrumb
         breadcrumb = ""
-        for bread in data_2['data']['breadcum']:
+        for bread in data['data']['breadcum']:
             breadcrumb = breadcrumb + bread['name'] + "/"
         data_render['breadcrumb'] = breadcrumb.strip("/")
         # Mandar los datos y construir los datos
         output = template.render(data=data_render)
         # declara donde generar el HTML
-        filename = f"generada/SendZoho-{id_product}.html"
-        if not os.path.exists("generada"):
-            os.makedirs("generada")
+        filename = f"build/SendZoho-{id_product}.html"
+        if not os.path.exists("build"):
+            os.makedirs("build")
         with open(filename, 'w') as f:
             f.write(output)
             f.close()
